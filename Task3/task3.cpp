80138248d46cb7bac590de1767f3a94efe7c9c13
Jesse Viitanen <essej.viitanen@gmail.com>
2020-11-19 15:31:45 +0100

    task3

--- Task3/task3.cpp
+++ Task3/task3.cpp
@@ -3,7 +3,46 @@ using namespace std;
 
 int main() {
 
-	cout << "Hello World" << endl;
+	int n;
+	
+	cout << "For loop" << endl;
+	cout << "Number: ";
+	cin >> n;
+
+	for(int i = 0; i < n; i++) {
+
+		cout << "Hello World" << endl;
+
+	}
+
+	cout << "\n--------------------------------------------------------------" << endl;
+	cout << "While loop" << endl;
+	cout << "Number: ";
+	cin >> n;
+
+	int i = 0;
+
+	while (i < n) {
+
+		cout << "Hello World" << endl;
+		i++;
+
+	}
+
+
+	cout << "\n--------------------------------------------------------------" << endl;
+	cout << "Do/while loop" << endl;
+	cout << "Number: ";
+	cin >> n;
+
+	i = 0;
+
+	do {
+
+		cout << "Hello World" << endl;
+		i++;
+
+	} while (i < n);
 
 	system("pause");
 	return 0;
