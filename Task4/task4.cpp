c3790212adbb03aafa621fff0627dafeca1c1d77
Jesse Viitanen <essej.viitanen@gmail.com>
2020-11-19 19:58:43 +0100

    task4

--- Task4/task4.cpp
+++ Task4/task4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -7,15 +8,50 @@ int main() {
 	int age;
 	string gender;
 	string adress;
-	bool married;
+	int adressnum;
+	bool married{};
 	float height;
 
 	cout << "Name: ";
 	cin >> name;
 
-	
+	cout << "\nAge: ";
+	cin >> age;
 
-	cout << name;
+	cout << "\nGender (m/f): ";
+	cin >> gender;
+
+	cout << "\nAdress: ";
+	cin >> adress;
+
+	cout << "\nAdress number: ";
+	cin >> adressnum;
+
+	string marriedq;
+	cout << "\nMarried? (true or false): ";
+	cin >> marriedq;
+	if (marriedq == "true") {
+		married = true;
+	}
+	else if (marriedq == "false") {
+		married = false;
+	}
+
+	cout << "\nHeight (In meters): ";
+	cin >> height;
+
+	cout << "\n";
+	cout << name << endl;
+	cout << age << " years old" << endl;
+	cout << gender << endl;
+	cout << adress << " " << adressnum << endl;
+	if (married == true) {
+		cout << "Married" << endl;
+	}
+	else if (married == false) {
+		cout << "Not married" << endl;
+	}
+	cout << height << " meters tall" << endl;
 
 	system("pause");
 	return 0;
