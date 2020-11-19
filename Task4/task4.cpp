cc9c331deb43d6b689b5c7b7c14221df469a0816
Jesse Viitanen <essej.viitanen@gmail.com>
2020-11-19 15:56:13 +0100

    task4

---
+++ Task4/task4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string name;
+	int age;
+	string gender;
+	string adress;
+	bool married;
+	float height;
+
+	cout << "Name: ";
+	cin >> name;
+
+	
+
+	cout << name;
+
+	system("pause");
+	return 0;
+}
