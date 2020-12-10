2149ade5d69cdcb54e673b28d75694cfeeb63c0e
Jesse Viitanen <essej.viitanen@gmail.com>
2020-12-11 00:29:31 +0100

    task6

--- Task6/task6.cpp
+++ Task6/task6.cpp
@@ -3,8 +3,37 @@ using namespace std;
 
 int main() {
 
-	
+	int n, m, num;
+	bool notprime = 0;
 
-	system("pause");
+	while (true)
+	{
+		system("cls");
+		cout << "How many prime numbers: ";
+		cin >> num;
+
+		for (int k = 2, x = 0; x < num; k++)
+		{
+			notprime = 0;
+			n = k;
+			m = n / 2;
+			for (int i = 2; i <= m; i++)
+			{
+				if (n % i == 0)
+				{
+					notprime = 1;
+					break;
+				}
+			}
+			if (notprime == 0)
+			{
+				cout << n << " ";
+				x++;
+			}
+		}
+		cout << "\n" << endl;
+		system("pause");
+	}
+
 	return 0;
 }
